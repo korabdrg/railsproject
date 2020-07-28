9a98863b78a61f83b3d72819b80ec431dc46191e
korabdrg <korabdrg@gmail.com>
2020-07-28 02:14:02 +0200

    Finished the CRUD form

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -3,4 +3,42 @@ class ArticlesController < ApplicationController
     def show
         @article = Article.find(params[:id])
     end
+
+    def index
+        @articles = Article.all
+    end
+
+    def new
+        @article = Article.new
+    end
+
+    def edit
+        @article = Article.find(params[:id])
+    end
+
+    def create
+        @article = Article.new(params.require(:article).permit(:title, :description))
+            if(@article.save)
+                flash[:notice] = "Article created!"
+                redirect_to article_path(@article)
+            else
+                render 'new'
+        end
+    end
+
+    def update
+        @article = Article.find(params[:id])
+            if @article.update(params.require(:article).permit(:title, :description))
+                flash[:notice] = "Updated!"
+                redirect_to article_path(@article)
+            else
+                render 'edit'
+            end
+    end
+
+    def destroy
+        @article = Article.find(params[:id])
+        @article.destroy
+        redirect_to articles_path
+    end
 end
