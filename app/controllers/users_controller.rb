a7102f8d9014d7fa160c7e459283b5643821802c
korabdrg <korabdrg@gmail.com>
2020-07-30 23:10:23 +0200

    Update user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,12 @@
 class UsersController < ApplicationController
+    before_action :find_user, only:[:show, :edit, :update, :destroy]
 
 def new 
     @user = User.new
+end
+
+def edit
+
 end
 
 def create
@@ -14,8 +19,21 @@ def create
     end
 end 
 
+def update
+    if @user.update(user_params)
+        flash[:notice] = "Updated!"
+        redirect_to articles_path
+    else
+        render 'edit'
+    end
+end
+
 private 
     def user_params
         params.require(:user).permit(:username, :email, :password)
+    end
+
+    def find_user
+        @user = User.find(params[:id])
     end
 end
