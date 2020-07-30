93be729e01d2860767d445c65073c767a2512784
korabdrg <korabdrg@gmail.com>
2020-07-30 02:35:25 +0200

    Add one to many association

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,11 @@
 class User < ApplicationRecord
-    
+    has_many :articles
+    validates :username, presence:true, 
+                         length:{minimum:3, maximum: 25}, 
+                         uniqueness:{ case_sensitive:false }
+    VALID_EMAIL_REGEX = (/\b[A-Z0-9._%a-z-]+@(?:[A-Z0-9a-z-]+.)+[A-Za-z]{2,4}\z/)
+    validates :email, presence:true,
+                      length:{maximum: 105}, 
+                      uniqueness:{ case_sensitive:false },
+                      format: {with: VALID_EMAIL_REGEX}
 end
