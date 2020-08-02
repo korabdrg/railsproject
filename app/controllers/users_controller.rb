75ae24edb31262fe9df697df71afca3050395e43
korabdrg <korabdrg@gmail.com>
2020-08-02 21:14:23 +0200

    idk

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,12 +6,19 @@ def new
 end
 
 def edit
+end
 
+def show
+end 
+
+def index
+    @users = User.paginate(page: params[:page], per_page: 5)
 end
 
 def create
     @user = User.new(user_params)
     if(@user.save)
+        session[:user_id] = @user.id
         flash[:notice] = "Registered!"
         redirect_to articles_path
     else
@@ -22,7 +29,7 @@ end
 def update
     if @user.update(user_params)
         flash[:notice] = "Updated!"
-        redirect_to articles_path
+        redirect_to @user
     else
         render 'edit'
     end
